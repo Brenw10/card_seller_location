76dd2b37fa03753d11fdffdf85ddf18f468a586c
Brendon Willian <bw.bwctm@gmail.com>
2019-04-04 18:30:18 -0300

    Renaming function

--- server/src/services/geolocation.js
+++ server/src/services/geolocation.js
@@ -1,9 +1,9 @@
 const haversine = require('haversine');
 
-function getDistancePointToPlacesAsc(point, places) {
+function getDistancePointToPlaces(point, places) {
   return places
     .map(place => ({ point: point.name, place: place.name, distance: haversine(point, place) }))
     .sort((a, b) => a.distance - b.distance);
 }
 
-module.exports = { getDistancePointToPlacesAsc };
+module.exports = { getDistancePointToPlaces };
--- server/src/services/sale.js
+++ server/src/services/sale.js
@@ -5,7 +5,7 @@ const geolocation = require('./geolocation');
 async function getWorkersByStores() {
   const workers = await worker.get();
   const stores = await store.get();
-  return stores.map(store => geolocation.getDistancePointToPlacesAsc(store, workers))
+  return stores.map(store => geolocation.getDistancePointToPlaces(store, workers))
     .map(list => list[0]);
 }
 
